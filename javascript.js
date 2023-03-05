c863509539923d155c8176935c51b74526d7f742
Jazz <JazzDevours@gmail.com>
2023-03-04 19:00:40 -0500

    add score tracking for player and computer

--- javascript.js
+++ javascript.js
@@ -18,43 +18,53 @@ function playRound() {
   const playerSelection = this.id;
   const computerSelection = getComputerChoice();
   if (playerSelection === computerSelection)
-    return results.textContent = "Draw! Try again.";
+    results.textContent = "Draw! Try again.";
     else if (playerSelection === "rock") {
     if (computerSelection === "paper") {
-      return results.textContent = "Paper beats Rock! You lose!";
+      computerScore++;
+      results.textContent = "Paper beats Rock! You lose!";
     }
     else {
-      return results.textContent = "Rock beats Scissors! You win!";
+      playerScore++;
+      results.textContent = "Rock beats Scissors! You win!";
     }
   }
   else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      return results.textContent = "Rock beats Paper! You win!";
+      playerScore++;
+      results.textContent = "Rock beats Paper! You win!";
     }
     else {
-      return results.textContent = "Scissors beats Paper! You lose!";
+      computerScore++;
+      results.textContent = "Scissors beats Paper! You lose!";
     }
   }
   else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
-      return results.textContent = "Rock beats Scissors! You lose!";
+      computerScore++;
+      results.textContent = "Rock beats Scissors! You lose!";
     }
     else {
-      return results.textContent = "Scissors beats Paper! You win!";
+      playerScore++;
+      results.textContent = "Scissors beats Paper! You win!";
     }
   }
+  scoreboard.textContent = `Player: ${playerScore} vs. Computer: ${computerScore}`;
 }
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
-  button.addEventListener('click', playRound)
+  button.addEventListener('click', playRound);
 });
+const scoreboard = document.querySelector('#scoreboard');
+let playerScore = 0;
+let computerScore = 0;
+scoreboard.textContent = `Player: ${playerScore} vs. Computer: ${computerScore}`;
 
 const results = document.querySelector('#results');
 
 /*function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+
   //for (let i = 0; i < 5; i++) {
    console.log(playerSelection + " vs " + computerSelection);
     if (playRound(playerSelection, computerSelection).includes("win")) {
